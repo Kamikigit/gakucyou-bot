ccb470781c08a658b35cabbb5c21b73daa77d306
Kamikigit <KCkc966@outlook.jp>
2019-08-10 12:25:00 +0000

    getting all links

--- getLink.py
+++ getLink.py
@@ -2,10 +2,23 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
-html = urlopen( "https://www.teu.ac.jp/gakucyou/index.html")
-soup = BeautifulSoup(html.read(), "html.parser")
-for link in soup.find("ul", {"class":"detail"}).find_all("a"):
-    if 'href' in link.attrs:
-        print(link.attrs['href'])
+pages = set()
 
-
+def getLinks(pageUrl):
+    global pages 
+    html = urlopen("https://www.teu.ac.jp" + pageUrl)
+    soup = BeautifulSoup(html.read(), "html.parser")
+    for link in soup.find("div", {"class":"mainarea"}).find_all("a", href = re.compile("^(/gakucyou/)")):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                newPage = link.attrs['href'] 
+                print(newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+               
+def serchLinks():
+    year = [i for i in range(2011, 2020)]
+    for i in range(9):
+        url = "/gakucyou/" + str(year[i]) + "/index.html"
+        getLinks(url)
+serchLinks()
