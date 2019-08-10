73075125e487f424a09dc1cbfe9f5d74ae4ead2a
Kamikigit <KCkc966@outlook.jp>
2019-08-10 14:06:49 +0000

    All problems are fixed.

--- getLink.py
+++ getLink.py
@@ -1,8 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
+from datetime import datetime
 
-pages = set()
+pages = [] 
 
 def getLinks(pageUrl):
     global pages 
@@ -12,13 +13,18 @@ def getLinks(pageUrl):
         if 'href' in link.attrs:
             if link.attrs['href'] not in pages:
                 newPage = link.attrs['href'] 
-                print(newPage)
-                pages.add(newPage)
+                #print(newPage)
+                pages.append(newPage)
                 getLinks(newPage)
                
 def serchLinks():
-    year = [i for i in range(2011, 2020)]
+    year = [i for i in range(2011, datetime.now().year)]
     for i in range(9):
         url = "/gakucyou/" + str(year[i]) + "/index.html"
         getLinks(url)
 serchLinks()
+
+#print(pages)
+
+
+
