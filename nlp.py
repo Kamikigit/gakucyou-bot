1e7dd7bfa4de8db7d7b244e3a3bad5b78eea06ac
Kamikigit <KCkc966@outlook.jp>
2019-08-18 08:05:35 +0000

    counting words

--- nlp.py
+++ nlp.py
@@ -1,4 +1,23 @@
 import sys
 import MeCab
-m = MeCab.Tagger("-Ochasen")
-print(m.parse("軽部征夫"))
+import re
+from collections import Counter
+
+with open("column.txt") as f:
+    data = f.read()
+
+mecab = MeCab.Tagger()
+parse = mecab.parse(data)
+lines = parse.split('\n')
+items = [re.split('[\t,]', line) for line in lines]
+words = [item[0]
+         for item in items 
+         if item[0] not in ('ESO', '')]
+
+counter = Counter(words)
+for word, cnt in counter.most_common():
+    with open("mustache.txt", "a") as f:
+        f.write(f"{word}:{cnt}")
+
+
+ 
