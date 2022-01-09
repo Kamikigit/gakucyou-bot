8a3fd6085c1c12ce59c3a12111fb3f465d6416f6
kamikigit <kamiki0906@gmail.com>
2022-01-10 01:42:18 +0900

    add exception

--- getLink.py
+++ getLink.py
@@ -10,6 +10,7 @@ def getLinks(pageUrl):
     html = urlopen("https://www.teu.ac.jp" + pageUrl)
     soup = BeautifulSoup(html.read(), "html.parser")
     for link in soup.find("div", {"class":"mainarea"}).find_all("a", href = re.compile("^(/gakucyou/)")):
+        #print(link)
         if 'href' in link.attrs:
             if link.attrs['href'] not in pages:
                 newPage = link.attrs['href'] 
@@ -19,12 +20,15 @@ def getLinks(pageUrl):
                
 def serchLinks():
     years = [year for year in range(2020, datetime.now().year+1)]
+    #当年度の記事がないとエラーが出る
+    #years = [2020, 2021]
     for year in years:
         url = "/gakucyou/" + str(year) + "/index.html"
+        #print("error", url)
         getLinks(url)
 serchLinks()
 
-#print(pages)
+#print("i",pages)
 
 
 
